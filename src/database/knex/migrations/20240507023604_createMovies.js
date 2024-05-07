febaa67bcfc05d5f0a7d0002c8c23d08222f09bf
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-07 00:20:14 -0300

    Adiciona constraint no banco para valor de rating

--- src/database/knex/migrations/20240507023604_createMovies.js
+++ src/database/knex/migrations/20240507023604_createMovies.js
@@ -2,7 +2,7 @@ exports.up = knex => knex.schema.createTable("movies", table => {
   table.increments("id").primary();
   table.text("title").notNullable();
   table.text("description").notNullable();
-  table.integer("rating");
+  table.integer("rating").checkBetween([0, 5]);
   table.integer("user_id").notNullable().references("id").inTable("users");
 
   table.timestamp("created_at").default(knex.fn.now());
