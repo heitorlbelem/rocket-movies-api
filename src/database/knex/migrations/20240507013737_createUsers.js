4e9f582ebe469366b32011be852c734718cdeb8a
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-06 23:39:34 -0300

    Corrige migration createUsers

--- src/database/knex/migrations/20240507013737_createUsers.js
+++ src/database/knex/migrations/20240507013737_createUsers.js
@@ -4,8 +4,8 @@ exports.up = knex => knex.schema.createTable("users", table => {
   table.text("email").notNullable().unique();
   table.text("password").notNullable();
 
-  table.timestamp("created_at").defaultTo(knex.fn.now());
-  table.timestamp("updated_at").defaultTo(knex.fn.now());
+  table.timestamp("created_at").default(knex.fn.now());
+  table.timestamp("updated_at").default(knex.fn.now());
 });
 
 exports.down = knex => knex.schema.dropTable("users");
