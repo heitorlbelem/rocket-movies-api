543e36b898ead6d59288939a3601614e6dd854eb
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-06 23:33:36 -0300

    Cria rota de atualização de dados do usuário

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,5 @@
 const knex = require("../database/knex");
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 
 class UsersController {
@@ -16,6 +16,48 @@ class UsersController {
     await knex("users").insert({ name, email, password: hashedPassword });
 
     response.status(201).json();
+  }
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { name, email, password, old_password } = request.body;
+
+    const user = await knex("users").where({ id }).first();
+    if(!user) {
+      throw new AppError("Usuário não encontrado");
+    }
+
+    const otherUserWithEmail = await knex("users").where({ email }).first();
+
+    if(otherUserWithEmail && (otherUserWithEmail.id !== user.id)) {
+      throw new AppError("E-mail já registrado para outro usuário");
+    }
+
+    if(password && !old_password) {
+      throw new AppError("Você deve fornecer a senha atual para definir uma nova senha");
+    }
+
+    if(password && old_password) {
+      const passwordMatch = await compare(old_password, user.password);
+      if(!passwordMatch) {
+        throw new AppError("Senha fornecida não confere");
+      }
+
+      user.password = await hash(password, 0);
+    }
+
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
+
+    await knex("users")
+      .where({ id })
+      .update({
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      });
+
+    response.json(user);
   }
 }
 
