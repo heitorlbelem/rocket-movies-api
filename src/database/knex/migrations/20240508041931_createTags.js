4434ec8acd1a69fe28061ff3d486c8edd0fe5a80
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-08 01:45:08 -0300

    Corrige migração das tags

--- src/database/knex/migrations/20240508041931_createTags.js
+++ src/database/knex/migrations/20240508041931_createTags.js
@@ -2,7 +2,7 @@ exports.up = knex => knex.schema.createTable("tags", table => {
   table.increments("id").primary();
   table.text("name").notNullable();
   table.integer("user_id").notNullable().references("id").inTable("users");
-  table.integer("note_id").notNullable().references("id").inTable("notes");
+  table.integer("movie_id").notNullable().references("id").inTable("movies");
 
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
