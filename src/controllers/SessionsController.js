4d88ef0d188f08aab9b85d220dd513805fc13ef4
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-17 20:52:23 -0300

    Cria rota para autenticação de usuário

---
+++ src/controllers/SessionsController.js
@@ -0,0 +1,31 @@
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
+const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
+
+class SessionsController {
+  async create(request, response) {
+    const { email, password } = request.body;
+
+    const user = await knex("users").where({ email }).first();
+    if(!user) {
+      throw new AppError("E-mail e/ou senhá inválidos", 401);
+    }
+    
+    const passwordMatched = await compare(password, user.password);
+    if(!passwordMatched) {
+      throw new AppError("E-mail e/ou senhá inválidos", 401);
+    }
+
+    const { expiresIn, secret } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn,
+    });
+
+    return response.json({ token });
+  }
+}
+
+module.exports = SessionsController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ const { Router } = require("express");
 const usersRoutes = require("./users.routes");
 const moviesRoutes = require("./movies.routes");
 const tagsRoutes = require("./tags.routes");
+const sessionsRoutes = require("./sessions.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/users/:user_id/movies", moviesRoutes);
 routes.use("/users/:user_id/tags", tagsRoutes);
+routes.use("/sessions", sessionsRoutes);
 
 module.exports = routes;
