75b1968ba29100092faec0f9e7e453f04d880ae2
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-17 21:44:52 -0300

    Corrige delete em cascata

--- src/database/knex/migrations/20240508041931_createTags.js
+++ src/database/knex/migrations/20240508041931_createTags.js
@@ -1,8 +1,18 @@
 exports.up = knex => knex.schema.createTable("tags", table => {
   table.increments("id").primary();
   table.text("name").notNullable();
-  table.integer("user_id").notNullable().references("id").inTable("users");
-  table.integer("movie_id").notNullable().references("id").inTable("movies");
+  table.integer("user_id")
+    .notNullable()
+    .references("id")
+    .inTable("users")
+    .onDelete("CASCADE")
+  ;
+  table.integer("movie_id")
+    .notNullable()
+    .references("id")
+    .inTable("movies")
+    .onDelete("CASCADE")
+  ;
 
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
